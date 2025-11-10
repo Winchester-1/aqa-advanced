af4b8bf7f7089e592cef4c618912d9e81aa2fcc4
Oleksandr Kozlovskyi <o.kozlovskyi@pnn.in.ua>
2025-11-10 18:53:46 +0200

    Add initial project files with chalk dependency and example usage

---
+++ nodeBasics/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Example of the green text'));
+console.log(chalk.red('Example of the red text'));
+console.log(chalk.blue('Example of the blue text'));
+console.log(chalk.yellow('Example of the yellow text'));
+console.log(chalk.black('Example of the black text'));
